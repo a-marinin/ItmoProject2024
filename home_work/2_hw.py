0bf8dd482549152443bce06eab7febd99941a856
amarinin <aleksandrqa1@gmail.com>
2024-05-30 21:43:55 +0300

    Fixed 2nd task

--- home_work/2_hw.py
+++ home_work/2_hw.py
@@ -8,22 +8,27 @@ def task_1():  # This is the 1st function
     print(var_int, var_float, var_str, var_list, var_bool) # Prints inside the function
     #return(var_int, var_float, var_str, var_list, var_bool) # Commit out to be able to print outside the function
 
+
 task_1()  # Calls the 1st function
-#print(task_1()) # Prints outside the 1st function (comment our the 9th row)
+# print(task_1()) # Prints outside the 1st function (comment our the 9th row)
 
 
-def task_2():  # This is the 3nd function
+def task_2() -> None:  # This is the 3rd function
     a: list = [1, 2, 3, 5, 8, 13, 21]
-    print(a[:3]) # Prints first 3 elements of the list
+    a_cut = a[:3]
+    print(a[:3])  # Prints first 3 elements of the list
     print('Числа Фибоначчи - элементы числовой последовательности, где каждое последующее число равно сумме двух предыдущих чисел.')
+    return a_cut
+
 
 task_2()  # Calls the 2nd function
 
 
 def task_3(a_number: int) -> int:  # This is the 3rd function
     square: int = a_number ** 2  # Square of the number
-    print(a_number, ' в квадрате - это ', square) # Prints inside the function
-    return (square)
+    print(a_number, ' в квадрате - это ', square)  # Prints inside the function
+    return square
 
-any_nuber: int = 77 # Задай любое число
-print(task_3(any_nuber)) # Распечатай в консоль квадрат этого числа
+
+any_nuber: int = 77  # Задай любое число
+print(task_3(any_nuber))  # Распечатай в консоль квадрат этого числа
