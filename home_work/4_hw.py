7fbe4c244cb0a372a586957769284f8876901947
amarinin <aleksandrqa1@gmail.com>
2024-06-02 22:20:10 +0300

    3rd homework, 2st task

--- home_work/4_hw.py
+++ home_work/4_hw.py
@@ -23,3 +23,35 @@ rectangle_3 = Rectangle(77, 99)
 print('1st rectangle ----> ', rectangle_1.square, rectangle_1.perimeter)
 print('2nd rectangle ----> ', rectangle_2.square, rectangle_2.perimeter)
 print('3rd rectangle ----> ', rectangle_3.square, rectangle_3.perimeter)
+
+class Math:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.addition = Math.addition(self, a, b)
+        self.multiplication = Math.multiplication(self, a, b)
+        self.division = Math.division(self, a, b)
+        self.subtraction = Math.subtraction(self, a, b)
+
+    def addition(self, a, b):
+        addition = a + b
+        return "The addition of 2 numbers ({} and {}) is equal to {}".format(a, b, addition)
+
+    def multiplication(self, a, b):
+        multiplication = a * b
+        return "The multiplication of 2 numbers ({} and {}) is equal to {}".format(a, b, multiplication)
+
+    def division(self, a, b):
+        division = a / b
+        return "The division of 2 numbers ({} and {}) is equal to {}".format(a, b, division)
+
+    def subtraction(self, a, b):
+        subtraction = a - b
+        return "The subtraction of 2 numbers ({} and {}) is equal to {}".format(a, b, subtraction)
+
+
+test = Math(25, 5)
+print(test.addition)
+print(test.multiplication)
+print(test.division)
+print(test.subtraction)
