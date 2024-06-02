cc3ce0180734c3cbcdae0fbe20fdcb00df2c18a0
amarinin <aleksandrqa1@gmail.com>
2024-06-02 19:44:29 +0300

    2nd homework, 2nd task

--- home_work/3_hw.py
+++ home_work/3_hw.py
@@ -1,3 +1,5 @@
+import calendar
+
 def get_greatest_number(number1: int, number2: int) -> int:
     print('The following numbers are passed to the function: ', number1, ' and ', number2)
     greatest_number = max(number1, number2)
@@ -16,5 +18,22 @@ def compare_2_numbers(number1: int, number2: int) -> None:
     if difference != 135:
         print('No. the difference between these numbers doesn\'t equals 135.')
 
-compare_2_numbers(77, 99)  # Result: No
-compare_2_numbers(-1, 134) # Result: Yez
+# compare_2_numbers(77, 99)  # Result: No
+# compare_2_numbers(-1, 134) # Result: Yes
+
+
+def get_season_by_month_number(month_number: int) -> None:
+
+    month_number_list = list(range(1, 13))  # The list of numeric months (1-12)
+    month_name_list = list(calendar.month_name[1:])  # The list of string months (Jan-Dec)
+    season_list = ('Winter ' * 2 + 'Spring ' * 3 + 'Summer ' * 3 + 'Autumn ' * 3 + 'Winter').split()  # The list of seasons (stirng)
+    result_list = [list(x) for x in zip(month_number_list, month_name_list, season_list)]  # The result list
+
+    print('You entered the following number: ', result_list[month_number-1][0])
+    print('It equals to the following month: ', result_list[month_number-1][1])
+    print('And this month is located is the following season: ', result_list[month_number-1][2])
+    print()
+
+
+for month in range(12):
+    get_season_by_month_number(month)
