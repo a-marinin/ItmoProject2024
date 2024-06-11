2a8360fc4e082c786d47b72c2d8e35c4f105cf14
amarinin <aleksandrqa1@gmail.com>
2024-06-11 12:24:50 +0300

    4th homework, 1st lesson's task

---
+++ lessons/task_9_oop.py
@@ -0,0 +1,19 @@
+class Button:
+
+    def __init__(self, text, link):
+        self.text = text
+        self.link = link
+
+
+# Создаём экземпляры класса
+home = Button('Домой', '/home')
+catalog_msk = Button('Каталог', '/msk/catalog')
+
+# Получаем доступ к атрибутам
+print(home.text)
+print('Кнопка ' + home.text + ' имеет ссылку ' + home.link)
+
+print('\n')
+
+print(catalog_msk.text)
+print('Кнопка ' + catalog_msk.text + ' имеет ссылку ' + catalog_msk.link)
