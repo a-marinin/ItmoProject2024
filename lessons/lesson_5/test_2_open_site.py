b2dd03643dbff73d2d92b204406fb3704d157168
amarinin <aleksandrqa1@gmail.com>
2024-06-11 18:54:34 +0300

    5th homework, 2nd lesson's task - test_2_open_site.py

--- lessons/lesson_5/test_2_open_site.py
+++ lessons/lesson_5/test_2_open_site.py
@@ -1,9 +1,18 @@
 '''
 Задача: Написать свой вервый тест.
 Тервый тест - это вход в Браузер
+Второй тест - это поиск элемета на странице
 '''
 
 from selenium import webdriver  # Импортируем webdriver из библиотеки selenium
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()  # Создаём объект драйвера
 driver.get('https://demoqa.com/')  # Вызываем метод get и ередаём в него URL-сайта
+
+# Поиск элемента на странице
+icon = driver.find_element(By.CSS_SELECTOR, 'header > a > img')
+if icon is None:
+    print('Элемент не найден')
+else:
+    print('Элекмент найден')
