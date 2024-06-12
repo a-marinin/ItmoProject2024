aab050d6bfd5ae05947e89de4f98fb18213da261
amarinin <aleksandrqa1@gmail.com>
2024-06-12 13:21:23 +0300

    7th classwork - 5th task

--- lessons/lesson_7/pages/demoqa.py
+++ lessons/lesson_7/pages/demoqa.py
@@ -14,13 +14,7 @@ class DemoQa(BasePage):
 
     def exist_icon(self):
         try:
-            self.find_element(locator='#app > header > a')
+            self.icon.find_element()
         except NoSuchElementException:
             return False
         return True
-
-    # def click_on_the_icon(self):
-    #     self.find_element(locator='#app > header > a').click()
-    #
-    # def click_on_the_btn(self):
-    #     self.find_element(locator='#app > div > div > div.home-body > div > div:nth-child(1)').click()
