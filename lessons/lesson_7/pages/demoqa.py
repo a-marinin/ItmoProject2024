0f74583df037ec2fde3f61a215d202b1a69b6062
amarinin <aleksandrqa1@gmail.com>
2024-06-12 13:07:45 +0300

    7th classwork - 4th task

--- lessons/lesson_7/pages/demoqa.py
+++ lessons/lesson_7/pages/demoqa.py
@@ -1,5 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
 from lessons.lesson_7.pages.base_page import BasePage
+from lessons.lesson_7.components.components import WebElement
 
 
 class DemoQa(BasePage):
@@ -8,6 +9,9 @@ class DemoQa(BasePage):
         self.base_url = 'https://demoqa.com/'
         super().__init__(driver, self.base_url)
 
+        self.icon = WebElement(driver, '#app > header > a')
+        self.btn_elements = WebElement(driver, '#app > div > div > div.home-body > div > div:nth-child(1)')
+
     def exist_icon(self):
         try:
             self.find_element(locator='#app > header > a')
@@ -15,8 +19,8 @@ class DemoQa(BasePage):
             return False
         return True
 
-    def click_on_the_icon(self):
-        self.find_element(locator='#app > header > a').click()
-
-    def click_on_the_btn(self):
-        self.find_element(locator='#app > div > div > div.home-body > div > div:nth-child(1)').click()
+    # def click_on_the_icon(self):
+    #     self.find_element(locator='#app > header > a').click()
+    #
+    # def click_on_the_btn(self):
+    #     self.find_element(locator='#app > div > div > div.home-body > div > div:nth-child(1)').click()
--- lessons/lesson_7/tests/test_go_to_page_elements.py
+++ lessons/lesson_7/tests/test_go_to_page_elements.py
@@ -7,5 +7,5 @@ def test_go_to_page_elements(browser):
 
     demo_qa_page.visit()
     assert demo_qa_page.equal_url()
-    demo_qa_page.click_on_the_btn()
+    demo_qa_page.btn_elements.click()
     assert elements_page.equal_url()
