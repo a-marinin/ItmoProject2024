ec2f1bb3b264d7e8fc8f4695d474f50f8e195e87
amarinin <aleksandrqa1@gmail.com>
2024-06-23 14:32:43 +0300

    8th classwork - All tasks are done.

--- lessons/lesson_8/pages/elements_page.py
+++ lessons/lesson_8/pages/elements_page.py
@@ -17,3 +17,8 @@ class ElementsPage(BasePage):
 
         self.text_center = WebElement(driver, '#app > div > div > div > div.col-12.mt-4.col-md-6')
         self.text_elements = WebElement(driver, 'dib.col-12:nth-child(2)')
+
+        self.icon = WebElement(driver, 'header > a > img')
+        self.btn_sidebar_first = WebElement(driver, 'div:nth-child(1) > span > div')
+        self.btn_sidebar_first_textbox = WebElement(driver, 'div:nth-child(1) > div > ul > #item-0 > span')
+        self.btn_sidebar_first_checkbox = WebElement(driver, 'div:nth-child(1) > div > ul > #item-1 > span')
--- lessons/lesson_8/tests/tests_hw/test_check_text.py
+++ lessons/lesson_8/tests/tests_hw/test_check_text.py
@@ -19,11 +19,15 @@ def test_check_element_text(browser):
     assert demo_qa_page.btn_elements.exist()  # Проверка того, что кнопка Elements существует
     demo_qa_page.btn_elements.click()  # Клик по кнопке Elements
     assert elements_page.equal_url()  # Проверка URL на соответствие demoqa.com/elements
-    assert elements_page.text_center.get_text() == 'Please select an item from left to start practice.'  # Проверка текста
+    assert elements_page.text_center.get_text() == 'Please select an item from left to start practice.'
 
 
 def test_page_elements(browser):
     # Проверка текста по центру на странице demoqa.com/elements (Перевод сразу на страницу Elements)
     el_page = ElementsPage(browser)
     el_page.visit()
-    assert el_page.text_elements.get_text() == 'Please select an item from left to start practice.'
+    assert el_page.text_elements.get_text() == 'Please select an item from left to start practice.'  # Из задания №1
+
+    assert el_page.icon.exist()  # Из задания №2
+    assert el_page.btn_sidebar_first.exist()  # Из задания №2
+    assert el_page.btn_sidebar_first_textbox.exist()  # Из задания №2
