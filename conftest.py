8b8c77d3d20427cc17b479573dd496493ea0d01f
amarinin <aleksandrqa1@gmail.com>
2024-06-27 00:20:17 +0300

    10th classwork - All homework tasks are done. Refactoring

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 @pytest.fixture(scope="session")
 def browser():
     driver = webdriver.Chrome()
+    browser.set_window_size(width=1000, height=1000)  # Установили размер браузера.
     yield driver  # Чтобы наш драйвер мог использоваться в функции нужно написать слово 'yield'
     driver.quit()
 
